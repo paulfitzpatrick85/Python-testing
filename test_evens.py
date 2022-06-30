7fca9826b2c598c630bdb0b19a2fb110e1d6351f
paulfitzpatrick85 <fitzer99@gmail.com>
2022-06-30 21:23:23 +0000

    the unittest library

---
+++ test_evens.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestEvens(unittest.TestCase):
+    pass  
+#  python expects code after ':', use pass when you have no code
+#  in a class, must pass in the 'self keyword'
+
+
+# when python runs a file directly it names it 'main
+if __name__ == "__main__":
+    unittest.main()
