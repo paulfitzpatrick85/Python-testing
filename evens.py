48aa6a588437f579e3a3d30ec430e7b608d5b670
paulfitzpatrick85 <fitzer99@gmail.com>
2022-06-30 22:04:33 +0000

    3.27 of Building a function using TDD

--- evens.py
+++ evens.py
@@ -1,11 +1,18 @@
-def even_number_of_evens(numbers):
-    return None
-    """
+"""
     Should Raise a TypeError if a list in not passed into the function
     error message: "A list was not passed into the function"
     if the list is empty it will return False
     if the number of even numbers is odd - return False
-    if the numner of even numbers is even - return True"""
+    if the numner of even numbers is even - return True
+    """
 
 
-print(even_number_of_evens(5))
+def even_number_of_evens(numbers):
+    if isinstance(numbers, list):
+        return True
+    else:
+        raise TypeError("A List was not passed into the function")
+
+
+if __name__ == "__main__":
+    print(even_number_of_evens(5))
--- test_evens.py
+++ test_evens.py
@@ -1,10 +1,20 @@
 import unittest
+from evens import even_number_of_evens
 
 
 class TestEvens(unittest.TestCase):
-    pass  
-#  python expects code after ':', use pass when you have no code
-#  in a class, must pass in the 'self keyword'
+
+    def test_throws_error_if_value_passed_in_is_not_list(self):
+        self.assertRaises(TypeError, even_number_of_evens, 4)  
+# self must be passed when using a class,
+# assertRaises is called from TestCase, check if typeError is thrown
+#  when funct is run with value of 4
+
+    def test_values_in_list(self):
+        self.assertEqual(even_number_of_evens([]), False)
+    # self.assertEqual(even_number_of_evens([2, 4]), True)
+    # self.assertEqual(even_number_of_evens([2]), False)
+    # self.assertEqual(even_number_of_evens([1, 3, 5]), False)
 
 
 # when python runs a file directly it names it 'main
