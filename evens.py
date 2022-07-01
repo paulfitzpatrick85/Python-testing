ae42982c326ee1f1c6395562ce3be0ccfc942d4a
paulfitzpatrick85 <fitzer99@gmail.com>
2022-07-01 19:32:59 +0000

    refactoring

--- evens.py
+++ evens.py
@@ -9,18 +9,21 @@
 
 def even_number_of_evens(numbers):
     if isinstance(numbers, list):
-        if numbers == []:
-            return False
-        else:
-            evens = 0
-            
-        for n in numbers:
-            if n % 2 == 0:
-                evens += 1
-        if evens:
-            return evens % 2 == 0  # will return true if number of 'evens' is even
-        else:
-            return False
+        # if numbers == []:  # line 24 covers this, so it can be deleted for D.R.Y's sake
+        #     return False
+        # else:
+        # evens = 0
+
+        evens = sum([1 for n in numbers if n % 2 == 0])   
+        # for n in numbers:  # can be replace with list comprehension above
+        #     if n % 2 == 0:
+        #         evens += 1
+
+        # if evens:
+        #     return evens % 2 == 0  # will return true if number of 'evens' is even
+        # else:
+        #     return False  #replaced with below
+        return True if evens and evens % 2 == 0 else False
     else:
         raise TypeError("A List was not passed into the function")
 
